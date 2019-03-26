c7a4f7385330d5e937181d006f16095001bb06ac
nickagliano <nickagliano@gmail.com>
2019-03-26 11:05:45 -0400

    upgraded the quiz for ranking items 1-4

--- js/index.js
+++ js/index.js
@@ -1,39 +1,107 @@
+var numbers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var q = 0;
+var w;
+
 var Quiz = function(){
   var self = this;
+
   this.init = function(){
     self._bindEvents();
   }
-  
-  this.correctAnswers = [
-    { question: 1, answer: 'a' },
-    { question: 2, answer: 'b' },
-    { question: 3, answer: 'd' },
-    { question: 4, answer: 'c' },
-    { question: 5, answer: 'd' },
-    { question: 6, answer: 'b' },
-  ]
-  
+
   this._pickAnswer = function($answer, $answers){
-    $answers.find('.quiz-answer').removeClass('active');
-    $answer.addClass('active');
+        if(numbers[q-1]==0){
+          $answer.addClass('active1');
+          numbers[q-1]++;
+        } else if (numbers[q-1]==1){
+            if($answer.hasClass('active1')){
+              $answer.removeClass('active1');
+              numbers[q-1]--;
+            } else {
+              $answer.addClass('active2');
+              numbers[q-1]++;
+            }
+        } else if (numbers[q-1]==2){
+          if ($answer.hasClass('active1')){
+            $answer.removeClass('active1');
+            numbers[q-1]--;
+            $answers.find('.quiz-answer.active2').addClass('active1'); //************************
+            $answers.find('.quiz-answer.active1').removeClass('active2'); //shifting answers!!!!!
+          } else if ($answer.hasClass('active2')){
+            $answer.removeClass('active2');//dont need to shift answers
+            numbers[q-1]--;
+          } else {
+            $answer.addClass('active3');
+            numbers[q-1]++;
+          }
+        } else if (numbers[q-1]==3){
+          if ($answer.hasClass('active1')){
+            $answer.removeClass('active1');
+            $answers.find('.quiz-answer.active2').addClass('active1'); //************************
+            $answers.find('.quiz-answer.active1').removeClass('active2'); //shifting answers!!!!!
+            $answers.find('.quiz-answer.active3').addClass('active2');
+            $answers.find('.quiz-answer.active3').removeClass('active3');
+            numbers[q-1]--;
+          } else if ($answer.hasClass('active2')){
+            $answer.removeClass('active2');
+            $answers.find('.quiz-answer.active3').addClass('active2'); //************************
+            $answers.find('.quiz-answer.active3').removeClass('active3'); //shifting answers!!!!!
+            numbers[q-1]--;
+          } else if ($answer.hasClass('active3')) {
+            $answer.removeClass('active3');
+            numbers[q-1]--;
+          } else {
+            $answer.addClass('active4');
+            numbers[q-1]++;
+            //show next question -- a possible function to add?
+          }
+        }
+          else if(numbers[q-1]==4){
+            if ($answer.hasClass('active1')){
+              $answer.removeClass('active1');
+              $answers.find('.quiz-answer.active2').addClass('active1'); //************************
+              $answers.find('.quiz-answer.active1').removeClass('active2'); //shifting answers!!!!!
+              $answers.find('.quiz-answer.active3').addClass('active2');
+              $answers.find('.quiz-answer.active3').removeClass('active3');
+              $answers.find('.quiz-answer.active4').addClass('active3');
+              $answers.find('.quiz-answer.active3').removeClass('active4');
+              numbers[q-1]--;
+            } else if ($answer.hasClass('active2')){
+              $answer.removeClass('active2');
+              $answers.find('.quiz-answer.active3').addClass('active2'); //************************
+              $answers.find('.quiz-answer.active3').removeClass('active3'); //shifting answers!!!!!
+              $answers.find('.quiz-answer.active4').addClass('active3');
+              $answers.find('.quiz-answer.active3').removeClass('active4');
+              numbers[q-1]--;
+            } else if ($answer.hasClass('active3')) {
+              $answer.removeClass('active3');
+              $answers.find('.quiz-answer.active4').addClass('active3');
+              $answers.find('.quiz-answer.active3').removeClass('active4');
+              numbers[q-1]--;
+            } else {
+              $answer.removeClass('active4');
+              numbers[q-1]--;
+            }
+          }
   }
+
   this._calcResult = function(){
     var numberOfCorrectAnswers = 0;
     $('ul[data-quiz-question]').each(function(i){
       var $this = $(this),
           chosenAnswer = $this.find('.quiz-answer.active').data('quiz-answer'),
           correctAnswer;
-      
+
       for ( var j = 0; j < self.correctAnswers.length; j++ ) {
         var a = self.correctAnswers[j];
         if ( a.question == $this.data('quiz-question') ) {
           correctAnswer = a.answer;
         }
       }
-      
+
       if ( chosenAnswer == correctAnswer ) {
         numberOfCorrectAnswers++;
-        
+
         // highlight this as correct answer
         $this.find('.quiz-answer.active').addClass('correct');
       }
@@ -42,16 +110,8 @@ var Quiz = function(){
         $this.find('.quiz-answer.active').addClass('incorrect');
       }
     });
-    if ( numberOfCorrectAnswers < 3 ) {
-      return {code: 'bad', text: 'Poor spelling skills'};
-    }
-    else if ( numberOfCorrectAnswers == 3 || numberOfCorrectAnswers == 4 ) {
-      return {code: 'mid', text: 'Moderate spelling skills'};
-    }
-    else if ( numberOfCorrectAnswers > 4 ) {
-      return {code: 'good', text: 'Good spelling skills'};
-    }
   }
+
   this._isComplete = function(){
     var answersComplete = 0;
     $('ul[data-quiz-question]').each(function(){
@@ -66,24 +126,27 @@ var Quiz = function(){
       return false;
     }
   }
+
   this._showResult = function(result){
     $('.quiz-result').addClass(result.code).html(result.text);
   }
+
   this._bindEvents = function(){
     $('.quiz-answer').on('click', function(){
       var $this = $(this),
           $answers = $this.closest('ul[data-quiz-question]');
+          q = $answers.data('quizQuestion');
       self._pickAnswer($this, $answers);
       if ( self._isComplete() ) {
-        
+
         // scroll to answer section
         $('html, body').animate({
           scrollTop: $('.quiz-result').offset().top
         });
-        
+
         self._showResult( self._calcResult() );
         $('.quiz-answer').off('click');
-        
+
       }
     });
   }
